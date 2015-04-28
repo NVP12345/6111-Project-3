4587655b5a3803a5dc6f65b53f35e87f5275f316
Nick Pizzoferrato <npizzoferrato@yodle.com>
2015-04-28 14:02:23 -0400

    adding different data set

--- README.txt
+++ README.txt
@@ -22,12 +22,12 @@ b)The following is a list of all the files that we are submitting:
                  example-run.txt
 
 
-c) We used the Stop & Frisk data from the NYPD for all five boroughs from 2014 as the source. To keep the size of the
-   data reasonable, we limited the data to a single month (May) and selected only somewhat interesting fields to
-   try and find some meaningful correlation (the 19 columns specified in dataset_gen.py). We used the script dataset_gen.py
-   to transform the source data into the desired CSV for the integrated dataset. This script parses the original data into
-   row objects with only the desired columns, which we then print based on the condition that the "datestop" begins with "5"
-   (indicating that the incident occurred in May). You can generate the integrated dataset using our script as follows:
+c) We used the NYS math test results by year, grade, district, and ethnicity as the source. To keep the size of the
+   data reasonable, we limited the data to a single year (2011) and grades (7 and 8) and selected only somewhat interesting
+   fields to try and find some meaningful correlation (the 9 columns specified in dataset_gen.py). We used the script
+   dataset_gen.py to transform the source data into the desired CSV for the integrated dataset. This script parses the
+   original data into row objects with only the desired columns, which we then print based on the condition that the
+   year is 2011 and the grade is 7 or 8. You can generate the integrated dataset using our script as follows:
 
         python dataset_gen.py dataset_source.csv > INTEGRATED-DATASET.csv
 
--- dataset_gen.py
+++ dataset_gen.py
@@ -4,117 +4,78 @@ import inspect
 from sets import Set
 
 class Row:
-    def __init__(self, datestop, pct, crimsusp, typeofid, arstmade, offunif, frisked, searched, contrabn, sex, race, agerange, htrange, weightrange, haircolor, eyecolor, build, premname, city):
+    def __init__(self, grade, year, category, meanscore, level1Pct, level2Pct, level3Pct, level4Pct, level3And4Pct):
         frame = inspect.currentframe()
         args, _, _, values = inspect.getargvalues(frame)
         for i in args:
             setattr(self, i, values[i])
+            setattr(self, 'print_' + i, i + "=" + str(values[i]))
 
-def compute_agerange(age):
+def compute_meanscore_range(meanscore):
     try:
-        age = int(age)
+        meanscore = int(meanscore)
     except ValueError, e:
         return 'Unknown'
 
-    if age < 20:
-        return '<20'
-    if age < 30:
-        return '20-29'
-    if age < 40:
-        return '30-39'
-    if age < 50:
-        return '40-49'
-    return '50+'
+    if meanscore < 675:
+        return '<675'
+    if meanscore < 700:
+        return '675-699'
+    return '700+'
 
-def compute_htrange(feet, inches):
-    total_inches = None
+def compute_pct_range(pct):
     try:
-        total_inches = 12 * int(feet) + int(inches)
+        pct = float(pct.replace('%', ''))
     except ValueError, e:
         return 'Unknown'
 
-    if total_inches < 60:
-        return "<5'0''"
-    if total_inches < 66:
-        return "5'0''- 5'5''"
-    if total_inches < 72:
-        return "5'5''- 5'11''"
-    if total_inches < 78:
-        return "6'0''- 6'5''"
-    return "6'6''+'"
-
-def compute_weightrange(weight):
-    try:
-        weight = int(weight)
-    except ValueError, e:
-        return 'Unknown'
-
-    if weight < 150:
-        return '<150'
-    if weight < 160:
-        return '150-159'
-    if weight < 170:
-        return '160-169'
-    if weight < 180:
-        return '170-179'
-    if weight < 190:
-        return '180-189'
-    if weight < 200:
-        return '190-199'
-    if weight < 225:
-        return '200-224'
-    if weight < 250:
-        return '225-249'
-    return '250+'
+    if pct < 10:
+        return '<10%'
+    if pct < 20:
+        return '10-20%'
+    if pct < 30:
+        return '20-30%'
+    if pct < 40:
+        return '30-40%'
+    if pct < 50:
+        return '40-50%'
+    if pct < 60:
+        return '50-60%'
+    if pct < 70:
+        return '60-70%'
+    if pct < 80:
+        return '70-80%'
+    if pct < 90:
+        return '80-90%'
+    return '90%+'
 
 def parse_row(row):
     return Row(
-        'datestop=' + row[3].strip(),
-        'pct=' + row[1].strip(),
-        'crimsusp=' + row[9].strip(),
-        'typeofid=' + row[11].strip(),
-        'arstmade=' + row[14].strip(),
-        'offunif=' + row[20].strip(),
-        'frisked=' + row[22].strip(),
-        'searched=' + row[23].strip(),
-        'contrabn=' + row[24].strip(),
-        'sex=' + row[80],
-        'race=' + row[81],
-        'agerange=' + compute_agerange(row[83].strip()),
-        'htrange=' + compute_htrange(row[84], row[85]),
-        'weightrange=' + compute_weightrange(row[86].strip()),
-        'haircolor=' + row[87].strip(),
-        'eyecolor=' + row[88].strip(),
-        'build=' + row[89].strip(),
-        'premname=' + row[94].strip(),
-        'city=' + row[100].strip()
+        row[1].strip(),
+        row[2].strip(),
+        row[3].strip(),
+        compute_meanscore_range(row[5].strip()),
+        compute_pct_range(row[7].strip()),
+        compute_pct_range(row[9].strip()),
+        compute_pct_range(row[11].strip()),
+        compute_pct_range(row[13].strip()),
+        compute_pct_range(row[15].strip())
     )
 
 def get_rows():
     with open(sys.argv[1], 'r') as lines:
-        return Set(filter((lambda x: x.datestop[:10] == 'datestop=5'), Set([parse_row(line.split(',')) for line in lines])))
+        return Set(filter((lambda x: x.year == '2011' and (x.grade == '7' or x.grade == '8')), Set([parse_row(line.split(',')) for line in lines])))
 
 def print_row(row):
-    print "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (
-        row.datestop,
-        row.pct,
-        row.crimsusp,
-        row.typeofid,
-        row.arstmade,
-        row.offunif,
-        row.frisked,
-        row.searched,
-        row.contrabn,
-        row.sex,
-        row.race,
-        row.agerange,
-        row.htrange,
-        row.weightrange,
-        row.haircolor,
-        row.eyecolor,
-        row.build,
-        row.premname,
-        row.city
+    print "%s, %s, %s, %s, %s, %s, %s, %s" % (
+        row.print_grade,
+        row.print_category,
+        row.print_meanscore,
+        row.print_level1Pct,
+        row.print_level2Pct,
+        row.print_level3Pct,
+        row.print_level4Pct,
+        row.print_level3And4Pct
     )
 
 if '__main__' == __name__:
